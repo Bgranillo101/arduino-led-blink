a9a2ac07bd94f1efbc22a5ec3b82994a9806c323
Brandon Granillo <130994502+Bgranillo101@users.noreply.github.com>
2026-01-04 16:56:00 -0600

    Add blink using millis()

---
+++ src/blink_millis.ino
@@ -0,0 +1,18 @@
+const int ledPin = 13;
+unsigned long previousMillis = 0;
+const long interval = 1000;
+bool ledState = LOW;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    ledState = !ledState;
+    digitalWrite(ledPin, ledState);
+  }
+}
